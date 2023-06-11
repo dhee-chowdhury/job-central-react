7e1dc7b54e40af3caf322d65399e5ff6b5c2213d
dhee-chowdhury <dhee.chowdhury@gmail.com>
2023-06-11 19:12:27 +0600

    added working filter jobs button

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -7,12 +7,35 @@ const AppliedJobs = () => {
   const savedJobs = useContext(AppliedJobsContext || []);
   console.log(savedJobs);
   const [appliedJobs, setAppliedJobs] = useState(savedJobs);
+
+  const handleShowAllJobs = () => {
+    setAppliedJobs(savedJobs);
+  };
+  const handleShowRemoteJobs = () => {
+    const remoteJobs = savedJobs.filter((sj) => sj.job_location === "remote");
+    if (remoteJobs) {
+      setAppliedJobs(remoteJobs);
+    }
+  };
+
+  const handleShowOnsiteJobs = () => {
+    const onSiteJobs = savedJobs.filter((sj) => sj.job_location === "onsite");
+    if (onSiteJobs) {
+      setAppliedJobs(onSiteJobs);
+    }
+  };
   return (
     <div className="my-container">
       <div className="flex gap-5 justify-end mb-4 lg:mb-8">
-        <button className="btn-outlined">All jobs</button>
-        <button className="btn-outlined">Remote jobs</button>
-        <button className="btn-outlined">Onsite jobs</button>
+        <button onClick={handleShowAllJobs} className="btn-outlined">
+          All jobs
+        </button>
+        <button onClick={handleShowRemoteJobs} className="btn-outlined">
+          Remote jobs
+        </button>
+        <button onClick={handleShowOnsiteJobs} className="btn-outlined">
+          Onsite jobs
+        </button>
       </div>
       <div>
         {appliedJobs.map((job) => (
