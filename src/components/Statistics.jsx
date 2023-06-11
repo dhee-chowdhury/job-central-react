a3e0576a3c77d4384254809fdac9a98d517d7468
dhee-chowdhury <dhee.chowdhury@gmail.com>
2023-06-12 02:36:08 +0600

    changed chart

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import {
-  BarChart,
+  ComposedChart,
+  Line,
+  Area,
   Bar,
-  Brush,
-  ReferenceLine,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
+  Scatter,
   ResponsiveContainer,
 } from "recharts";
 
@@ -26,19 +27,23 @@ const Statistics = () => {
   return (
     <div className="my-container">
       <div>
-        <h1 className="title-text text-3xl font-extrabold">Assignment Marks</h1>
+        <h1 className="title-text text-3xl font-extrabold text-center mb-5">
+          Assignment Marks
+        </h1>
       </div>
-      <div>
-        <BarChart width={1200} height={500} data={data}>
-          {/* <CartesianGrid strokeDasharray="3 3" /> */}
+      <div className="flex items-center justify-center">
+        <ComposedChart width={1000} height={300} data={data}>
+          <CartesianGrid stroke="#f5f5f5" />
           <XAxis dataKey="name" />
           <YAxis dataKey="total_marks" />
           <Tooltip />
-          <Legend verticalAlign="top" wrapperStyle={{ lineHeight: "40px" }} />
-
-          <Bar dataKey="obtained_marks" fill="#9873FF" />
-          <Bar dataKey="total_marks" fill="#7E90FE" />
-        </BarChart>
+          <Legend />
+          <Bar dataKey="obtained_marks" barSize={20} fill="#9873FF" />
+          <Bar dataKey="total_marks" barSize={20} fill="#7E90FE" />
+          <Line type="monotone" dataKey="obtained_marks" stroke="#ff7300" />
+          <Line type="monotone" dataKey="total_marks" stroke="#22c55e" />
+          {/* <Scatter dataKey="cnt" fill="red" /> */}
+        </ComposedChart>
       </div>
     </div>
   );
